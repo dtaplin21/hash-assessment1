4d4ef59a6e4f5d4eb4a6b8dbd64cc8006634ecc3
Dom <dtaplin21@aol.com>
2023-06-10 11:04:41 -0700

    binary

--- problems/binary-to-string.js
+++ problems/binary-to-string.js
@@ -1,10 +1,17 @@
 function binaryToString(binaryBlob) {
-  // Your code here
+let str = "";
+for(let i = 0; i < binaryBlob.length; i += 8) {
+const binary = binaryBlob.slice(i, i + 8);
+const decimal = parseInt(binary, 2);
+const newStr = String.fromCharCode(decimal);
+str += newStr
+ }
+ return str
 }
 
 /* Comment in the code below to run local tests */
-// console.log(binaryToString('010000010100001001000011')); // 'ABC'
-// console.log(binaryToString('001101100011011100111000')); // '678'
+console.log(binaryToString('010000010100001001000011')); // 'ABC'
+console.log(binaryToString('001101100011011100111000')); // '678'
 
 
 module.exports = binaryToString;
